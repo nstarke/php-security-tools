04ba338ce44cec8c01930252b37c1767ca469d62
Nicholas Starke <starke.nicholas@gmail.com>
2015-08-04 17:01:43 -0500

    cleaning up validation messages for netcat.php

--- netcat.php
+++ netcat.php
@@ -68,11 +68,11 @@ if (array_key_exists('command-shell', $_GET)) {
 $startErrors = [];
 
 if ($listenPort != 0 && $targetPort != 0) {
-    $startErrors[] = "Listen and target ports cannot be set simultaneously.";
+    $startErrors[] = 'Listen and target ports cannot be set simultaneously.\n';
 }
 
 if ($listenPort != 0 && strlen($targetHost) != 0) {
-    $startErrors[] = "Listen port and target host cannot be set simultaneously";
+    $startErrors[] = 'Listen port and target host cannot be set simultaneously.\n';
 }
 
 if (count($startErrors) > 0){
